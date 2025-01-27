10954b06dd53f29a5f2fe33150aab1306bab3a84
Aradhna Patel <aradhnaen1009@gmail.com>
2025-01-27 16:12:25 +0530

    fix: lint error(more errors)

--- model.py
+++ model.py
@@ -1,9 +1,9 @@
 # model.py
 import pandas as pd
-from sklearn.datasets import load_iris
+# from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split, GridSearchCV
 
-from sklearn.ensemble import RandomForestClassifier
+# from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 import joblib
@@ -13,13 +13,13 @@ import mlflow.sklearn
 # Load the dataset
 iris = pd.read_csv("./data/iris.csv")
 X = iris.drop('variety', axis=1)
-y = iris['variety'] 
+y = iris['variety']
 
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
-mlflow.set_tracking_uri("http://localhost:5000") 
+mlflow.set_tracking_uri("http://localhost:5000")
 # Train the model
 # model = RandomForestClassifier()
 mlflow.set_experiment("log experiments")
@@ -41,7 +41,8 @@ param_grid = {
 }
 
 # # Perform GridSearchCV
-# grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=5, n_jobs=-1, verbose=2)
+# grid_search = GridSearchCV(
+# estimator=model, param_grid=param_grid, cv=5, n_jobs=-1, verbose=2)
 # grid_search.fit(X_train, y_train)
 
 # # Get best hyperparameters and model
@@ -54,24 +55,21 @@ param_grid = {
 # model.fit(X_train, y_train)
 with mlflow.start_run():
     mlflow.log_param("dataset", "iris")
-    mlflow.log_param("model", "SVC")
-    
+    mlflow.log_param("model", "SVC")    
     # Perform GridSearchCV to find the best hyperparameters
-    grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=5, n_jobs=-1, verbose=2)
+    grid_search = GridSearchCV(
+        estimator=model, param_grid=param_grid, cv=5, n_jobs=-1, verbose=2
+    )
     grid_search.fit(X_train, y_train)
-    
     # Log the best parameters found
     best_params = grid_search.best_params_
-    mlflow.log_params(best_params)
-    
+    mlflow.log_params(best_params)    
     # Get the best model and its accuracy
     best_model = grid_search.best_estimator_
     y_pred = best_model.predict(X_test)
     # accuracy = accuracy_score(y_test, y_pred)
-    
     # Log the accuracy of the model
     # mlflow.log_metric("accuracy", accuracy)
-    
     # Log the trained model
     # mlflow.sklearn.log_model(best_model, "model")
     # Make predictions and evaluate the model
